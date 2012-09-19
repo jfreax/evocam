3fd6f2bea831f31472c9492a1a14036f98852e65
Jens Dieskau <jfreax@googlemail.com>
2012-09-19 23:49:28 +0200

    Add fabric config file (to deploy)

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+from __future__ import with_statement
+from fabric.api import run, local, cd
+
+
+def push():
+    local("git push")
+    
+def deploy():
+    push()
+    
+    code_dir = '/var/www/static/evoface'
+    with cd(code_dir):
+        run("git pull")
+        run("coffee -c *.coffee")
