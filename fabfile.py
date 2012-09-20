f571feb20f89662c1a6acc1f6a66102300f0d085
Jens Dieskau <jfreax@googlemail.com>
2012-09-20 16:43:19 +0200

    Rearanged directories and necessary modification to the fab file.

--- fabfile.py
+++ fabfile.py
@@ -2,14 +2,28 @@ from __future__ import with_statement
 from fabric.api import run, local, cd, env
 
 env.hosts = ['jdsoft.de']
+src_dir = '/home/jens/VCS/evoface'
+public_dir = '/var/www/static/evoface'
 
+
+# Local #
 def push():
     local("git push")
+
     
+def make(watch=False):
+    local("coffee " + ("-w " if watch else "") +"-o public/js -c src/*.coffee")
+
+# Remote #
 def deploy():
     push()
     
-    code_dir = '/var/www/static/evoface'
-    with cd(code_dir):
+    with cd(public_dir):
         run("git pull")
-        run("coffee -c *.coffee")
+        run("fab make")
+        run("cp -r " + src_dir + "/public/* " + public_dir + "/")
+    
+# Helper #
+def server():
+    make()
+    local("cd public && python -m SimpleHTTPServer")
